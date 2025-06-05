557ffbae3aa02ad392070ba821a907ba1c5495f6
Ujvari Gyorgy <ujvari.gyorgy@gmail.com>
2025-06-05 16:37:33 +0300

    fix: request fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import dogRoutes from "./routes/dogRoutes";
 import kennelRoutes from "./routes/kennelRoutes";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +28,6 @@ mongoose
 app.use("/api/dogs", dogRoutes);
 app.use("/api/kennels", kennelRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
